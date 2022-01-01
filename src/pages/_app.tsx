8235af47067ecc9299a1001e61ffbf551fc1cc41
Zechariah <zechariahtan144@gmail.com>
2022-01-02 01:11:40 +0800

    Did Web Admin (PWA, Open Graph)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 			<Head>
 				<title>Zechariah Tan</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="favicon.ico" />
+				<link rel="apple-touch-icon" href="favicon.ico"></link>
+				<link rel="manifest" href="manifest.json" />
+				<meta name="theme-color" content="#000000" />
+				<meta name="title" content="Zechariah Tan" />
+				<meta name="description" content="Zechariah's Portfolio Website" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="http://www.zectan.com/" />
+				<meta property="og:title" content="Zechariah Tan" />
+				<meta property="og:description" content="Zechariah's Portfolio Website" />
+				<meta property="og:image" content="http://www.zectan.com/assets/profile.jpg" />
 			</Head>
 			<Component {...pageProps} />
 		</>
