be901937457393449d952a931e56ef50439f679f
zS1L3NT Windows <zechariahtan144@gmail.com>
2023-08-08 01:30:21 +0800

    ✨ group filter tags by categories

--- src/features/projects/FiltersModal.tsx
+++ src/features/projects/FiltersModal.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
-import { HIDDEN_TAGS, PNG_TAGS } from "@/constants"
+import { HIDDEN_TAGS, PNG_TAGS, TAG_CATEGORIES } from "@/constants"
 
 export default function FiltersModal({
 	tags,
@@ -20,6 +20,13 @@ export default function FiltersModal({
 		setSelectedTags(searchTags)
 	}, [searchTags])
 
+	const otherTags = tags.filter(
+		t =>
+			!TAG_CATEGORIES.map(c => c[1])
+				.flat()
+				.includes(t) && !HIDDEN_TAGS.includes(t),
+	)
+
 	return (
 		<div
 			id="filters-modal"
@@ -33,44 +40,50 @@ export default function FiltersModal({
 					<h1 className="xs:text-xl sm:text-2xl lg:text-3xl w-fit font-montserrat-bold">
 						Filters
 					</h1>
-					<div className="grid flex-1 overflow-y-scroll xs:gap-2 sm:gap-3 lg:gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xs:mt-4 sm:mt-6 lg:mt-8">
-						{tags
-							.filter(t => !HIDDEN_TAGS.includes(t))
-							.sort()
-							.map(t => (
-								<label
-									key={t}
-									htmlFor={t + "-checkbox"}
-									className="flex items-center cursor-pointer">
-									<input
-										id={t + "-checkbox"}
-										type="checkbox"
-										checked={selectedTags.includes(t)}
-										onChange={() =>
-											setSelectedTags(st =>
-												st.includes(t)
-													? st.filter(tag => tag !== t)
-													: [...st, t],
-											)
-										}
-										className="w-4 h-4 rounded accent-primary-400"
-									/>
-									<Image
-										key={t}
-										title={t[0]!.toUpperCase() + t.substring(1)}
-										className="inline-block mx-2 xs:scale-75 sm:scale-90"
-										src={`https://res.cloudinary.com/zs1l3nt/image/upload/icons/${t}.${
-											PNG_TAGS.includes(t) ? "png" : "svg"
-										}`}
-										alt={t + " icon"}
-										width={25}
-										height={25}
-									/>
-									<p className="xs:text-sm sm:text-base lg:text-md font-montserrat-regular">
-										{t[0]!.toUpperCase() + t.slice(1)}
-									</p>
-								</label>
-							))}
+					<div className="flex flex-col flex-1 overflow-x-hidden overflow-y-scroll xs:mt-4 sm:mt-6 lg:mt-8 xs:gap-4 sm:gap-5 lg:gap-6">
+						{[...TAG_CATEGORIES, ["Other", otherTags] as const].map(([name, tags]) => (
+							<div key={name}>
+								<h1 className="xs:mb-1 sm:mb-1.5 lg:mb-2 xs:text-sm sm:text-base lg:text-md font-montserrat-bold">
+									{name}
+								</h1>
+								<div className="grid xs:gap-2 sm:gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+									{tags.sort().map(t => (
+										<label
+											key={t}
+											htmlFor={t + "-checkbox"}
+											className="flex items-center cursor-pointer">
+											<input
+												id={t + "-checkbox"}
+												type="checkbox"
+												checked={selectedTags.includes(t)}
+												onChange={() =>
+													setSelectedTags(st =>
+														st.includes(t)
+															? st.filter(tag => tag !== t)
+															: [...st, t],
+													)
+												}
+												className="w-4 h-4 rounded accent-primary-400"
+											/>
+											<Image
+												key={t}
+												title={t[0]!.toUpperCase() + t.substring(1)}
+												className="inline-block mx-2 xs:scale-75 sm:scale-90"
+												src={`https://res.cloudinary.com/zs1l3nt/image/upload/icons/${t}.${
+													PNG_TAGS.includes(t) ? "png" : "svg"
+												}`}
+												alt={t + " icon"}
+												width={25}
+												height={25}
+											/>
+											<p className="xs:text-sm sm:text-base lg:text-md font-montserrat-regular">
+												{t[0]!.toUpperCase() + t.slice(1)}
+											</p>
+										</label>
+									))}
+								</div>
+							</div>
+						))}
 					</div>
 					<div className="flex justify-end gap-4 px-4 mt-6">
 						<button
