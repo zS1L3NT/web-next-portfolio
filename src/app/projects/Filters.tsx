19eb225cf1220f04baf8a40a007183224076fa08
zS1L3NT Mac <zechariahtan144@gmail.com>
2024-02-22 21:18:29 +0800

    ✨ add back filters on projects page

---
+++ src/app/projects/Filters.tsx
@@ -0,0 +1,180 @@
+"use client"
+
+import { Modal } from "flowbite"
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+import { HIDDEN_TAGS, TAG_CATEGORIES } from "@/constants"
+
+type Props = {
+	searchParams: Record<string, string>
+	tags: string[]
+}
+
+function FiltersModal({
+	tags,
+	searchTags,
+	getTagsLink,
+}: {
+	tags: string[]
+	searchTags: string[]
+	getTagsLink: (tags: string[]) => string
+}) {
+	const [selectedTags, setSelectedTags] = useState<string[]>(searchTags)
+
+	useEffect(() => {
+		setSelectedTags(searchTags)
+	}, [searchTags])
+
+	const otherTags = tags.filter(
+		t =>
+			!TAG_CATEGORIES.map(c => c[1])
+				.flat()
+				.includes(t) && !HIDDEN_TAGS.includes(t),
+	)
+
+	return (
+		<div
+			id="filters-modal"
+			tabIndex={-1}
+			aria-hidden="true"
+			data-modal-backdrop="static"
+			data-modal-placement="top-center"
+			className="fixed top-0 left-0 right-0 z-50 hidden w-full h-full overflow-x-hidden overflow-y-auto xs:p-4 sm:p-6 lg:p-8 backdrop-brightness-75 backdrop-blur-sm md:inset-0">
+			<div className="relative w-full h-full max-w-3xl">
+				<div className="flex flex-col h-full shadow-md xs:p-4 sm:p-5 lg:p-6 bg-slate-200">
+					<h1 className="xs:text-xl sm:text-2xl lg:text-3xl w-fit font-montserrat-bold">
+						Filters
+					</h1>
+					<div className="flex flex-col flex-1 overflow-x-hidden overflow-y-scroll xs:mt-4 sm:mt-6 lg:mt-8 xs:gap-4 sm:gap-5 lg:gap-6">
+						{[...TAG_CATEGORIES, ["Other", otherTags] as const].map(([name, tags]) => (
+							<div key={name}>
+								<h1 className="xs:mb-1 sm:mb-1.5 lg:mb-2 xs:text-sm sm:text-base lg:text-md font-montserrat-bold">
+									{name}
+								</h1>
+								<div className="grid xs:gap-2 sm:gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+									{tags.sort().map(t => (
+										<label
+											key={t}
+											htmlFor={t + "-checkbox"}
+											className="flex items-center cursor-pointer">
+											<input
+												id={t + "-checkbox"}
+												type="checkbox"
+												checked={selectedTags.includes(t)}
+												onChange={() =>
+													setSelectedTags(st =>
+														st.includes(t)
+															? st.filter(tag => tag !== t)
+															: [...st, t],
+													)
+												}
+												className="w-4 h-4 rounded accent-primary-400"
+											/>
+											<Image
+												key={t}
+												title={t[0]!.toUpperCase() + t.substring(1)}
+												className="inline-block mx-2 xs:scale-75 sm:scale-90"
+												src={`https://res.cloudinary.com/zs1l3nt/image/upload/icons/${t}.svg`}
+												alt={t + " icon"}
+												width={25}
+												height={25}
+											/>
+											<p className="xs:text-sm sm:text-base lg:text-md font-montserrat-regular">
+												{t[0]!.toUpperCase() + t.slice(1)}
+											</p>
+										</label>
+									))}
+								</div>
+							</div>
+						))}
+					</div>
+					<div className="flex justify-end gap-4 px-4 mt-6">
+						<button
+							onClick={() =>
+								new Modal(document.getElementById("filters-modal")).hide()
+							}
+							className="block px-3 py-2 border text-primary-400 xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 border-primary-400">
+							Cancel
+						</button>
+						<Link
+							href={getTagsLink(selectedTags)}
+							onClick={() =>
+								new Modal(document.getElementById("filters-modal")).hide()
+							}
+							className="block px-3 py-2 text-white xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 hover:shadow-primary-400 bg-primary-400">
+							Save
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default function FiltersButton({ searchParams, tags }: Props) {
+	const search = new URLSearchParams(searchParams)
+	const searchTags = (search.get("tags")?.split(",") ?? []).filter(t => tags.includes(t))
+
+	const getTagsLink = (tags: string[]) => {
+		const newSearch = new URLSearchParams(search.toString())
+
+		if (tags.length) {
+			newSearch.set("tags", tags.join(","))
+		} else {
+			newSearch.delete("tags")
+		}
+		newSearch.delete("page")
+
+		const newSearchString = newSearch.toString().replace(/%2C/g, ",")
+		return "/projects" + (newSearchString.length ? "?" + newSearchString : "")
+	}
+
+	return (
+		<div className="flex xs:gap-2 sm:gap-3 lg:gap-4 xs:mb-8 sm:mb-10 lg:mb-12">
+			<button
+				className="flex items-center justify-center shadow-md cursor-pointer hover:scale-105 xs:text-xs sm:text-sm xs:p-2 sm:p-3 hover:shadow-slate-300 shadow-slate-200 bg-slate-200 font-montserrat-regular"
+				type="button"
+				onClick={() => new Modal(document.getElementById("filters-modal")).toggle()}>
+				<Image
+					src="/assets/images/filter.png"
+					alt="Filter"
+					width={16}
+					height={16}
+				/>
+			</button>
+
+			<FiltersModal
+				tags={tags}
+				searchTags={searchTags}
+				getTagsLink={getTagsLink}
+			/>
+
+			{searchTags.map(t => (
+				<div
+					key={t}
+					className="flex items-center gap-2 shadow-md xs:text-xs sm:text-sm xs:py-2 sm:py-3 xs:px-3 sm:px-4 font-montserrat-regular bg-slate-200">
+					<Image
+						title={t[0]!.toUpperCase() + t.slice(1)}
+						className="inline-block"
+						src={`https://res.cloudinary.com/zs1l3nt/image/upload/icons/${t}.svg`}
+						alt={t + " icon"}
+						width={16}
+						height={16}
+					/>
+					{t[0]!.toUpperCase() + t.slice(1)}
+					<Link href={getTagsLink(searchTags.filter(t_ => t !== t_))}>
+						<Image
+							className="ml-1 cursor-pointer hover:scale-105"
+							src="/assets/images/close.png"
+							alt="Filter"
+							width={13}
+							height={13}
+						/>
+					</Link>
+				</div>
+			))}
+		</div>
+	)
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,65 +1,12 @@
-import Image from "next/image"
-import Link from "next/link"
-
-import { HIDDEN_TAGS, SPECIAL_TAGS } from "@/constants"
 import getProjects from "@/utils/getProjects"
 
-export default async function Page() {
-	const projects = await getProjects()
+import ClientPage from "./client"
 
+export default async function Page({ searchParams }: { searchParams: Record<string, string> }) {
 	return (
-		<main className="container mx-auto xs:px-4 sm:px-6 xs:py-8 sm:py-12 lg:py-16">
-			<Link
-				href="/"
-				className="inline-block hover:scale-105 font-montserrat-regular xs:text-sm sm:text-base lg:text-md">
-				&larr; Home
-			</Link>
-			<h1 className="xs:mt-2 sm:mt-4 lg:mt-6 xs:text-3xl sm:text-4xl lg:text-5xl w-fit font-montserrat-bold">
-				Projects
-			</h1>
-			<div className="container mx-auto xs:my-6 sm:my-9 md:my-14">
-				<div className="grid xs:gap-8 sm:gap-10 lg:gap-12 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-					{projects.map(project => (
-						<div
-							key={project.title}
-							className="flex flex-col justify-between h-full gap-4 shadow-md xs:p-4 sm:p-5 lg:p-6 shadow-slate-100 bg-slate-100 ">
-							<div>
-								<h1 className="xs:text-md sm:text-lg lg:text-xl font-montserrat-bold">
-									{project.title}
-									{SPECIAL_TAGS.filter(t => project.tags.includes(t[0])).map(
-										([tag, emoji, message]) => (
-											<span
-												key={tag}
-												title={message}
-												className="inline-block ms-1 hover:scale-125">
-												{emoji}
-											</span>
-										),
-									)}
-								</h1>
-								<p className="mt-1 font-montserrat-regular xs:text-sm sm:text-base lg:text-md">
-									{project.description}
-								</p>
-							</div>
-							<div className="flex flex-wrap xs:gap-1 sm:gap-2 lg:gap-3">
-								{project.tags
-									.filter(t => !HIDDEN_TAGS.includes(t))
-									.map(t => (
-										<Image
-											key={t}
-											title={t[0]!.toUpperCase() + t.substring(1)}
-											className="inline-block xs:scale-75 sm:scale-90"
-											src={`https://res.cloudinary.com/zs1l3nt/image/upload/icons/${t}.svg`}
-											alt={t + " icon"}
-											width={25}
-											height={25}
-										/>
-									))}
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-		</main>
+		<ClientPage
+			searchParams={searchParams}
+			projects={await getProjects()}
+		/>
 	)
 }
