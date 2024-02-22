2ce7b42e7f3073b282df3cbf1835f50db97479e1
zS1L3NT Mac <zechariahtan144@gmail.com>
2024-02-23 00:43:04 +0800

    ♻️ refactor filters bar structure

--- src/app/projects/Filters.tsx
+++ src/app/projects/Filters.tsx
@@ -82,69 +82,70 @@ export default function Filters({ tags, searchTags }: { tags: string[]; searchTa
 				className={cn(
 					"fixed inset-0 backdrop-brightness-75 backdrop-blur-sm size-full",
 					isOpen ? "opacity-100 z-[1000]" : "opacity-0 -z-[1]",
-				)}
-			/>
+				)}>
+				<AnimatePresence>
+					{isOpen ? (
+						<motion.div
+							initial={{ y: -20 }}
+							animate={{ y: 0 }}
+							exit={{ y: -20 }}
+							className="size-full xs:p-4 sm:p-6 md:p-8 no-transition"
+							onClick={() => setIsOpen(false)}>
+							<div
+								className="flex flex-col max-w-3xl mx-auto shadow-md bg-slate-200 size-full xs:p-4 sm:p-5 lg:p-6"
+								onClick={e => e.stopPropagation()}>
+								<h1 className="xs:text-xl sm:text-2xl lg:text-3xl w-fit font-montserrat-bold">
+									Filters
+								</h1>
 
-			<AnimatePresence>
-				{isOpen ? (
-					<motion.div
-						initial={{ opacity: 0, y: -20 }}
-						animate={{ opacity: 1, y: 0 }}
-						exit={{ opacity: 0, y: -20 }}
-						className="fixed inset-0 size-full z-[1001] xs:p-4 sm:p-6 md:p-8 no-transition"
-						onClick={() => setIsOpen(false)}>
-						<div
-							className="flex flex-col max-w-3xl mx-auto shadow-md bg-slate-200 size-full xs:p-4 sm:p-5 lg:p-6"
-							onClick={e => e.stopPropagation()}>
-							<h1 className="xs:text-xl sm:text-2xl lg:text-3xl w-fit font-montserrat-bold">
-								Filters
-							</h1>
-
-							<div className="flex flex-col flex-1 overflow-x-hidden overflow-y-scroll xs:mt-4 sm:mt-6 lg:mt-8 xs:gap-4 sm:gap-5 lg:gap-6">
-								{[...TAG_CATEGORIES, ["Other", OTHER_TAGS] as const].map(
-									([name, tags]) => (
-										<div key={name}>
-											<h1 className="xs:mb-1 sm:mb-1.5 lg:mb-2 xs:text-sm sm:text-base lg:text-md font-montserrat-bold">
-												{name}
-											</h1>
-											<div className="grid xs:gap-2 sm:gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-												{[...tags].sort().map(t => (
-													<Checkbox
-														key={t}
-														tag={t}
-														checked={selectedTags.includes(t)}
-														onChange={() =>
-															setSelectedTags(st =>
-																st.includes(t)
-																	? st.filter(tag => tag !== t)
-																	: [...st, t],
-															)
-														}
-													/>
-												))}
+								<div className="flex flex-col flex-1 overflow-x-hidden overflow-y-scroll xs:mt-4 sm:mt-6 lg:mt-8 xs:gap-4 sm:gap-5 lg:gap-6">
+									{[...TAG_CATEGORIES, ["Other", OTHER_TAGS] as const].map(
+										([name, tags]) => (
+											<div key={name}>
+												<h1 className="xs:mb-1 sm:mb-1.5 lg:mb-2 xs:text-sm sm:text-base lg:text-md font-montserrat-bold">
+													{name}
+												</h1>
+												<div className="grid xs:gap-2 sm:gap-3 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+													{[...tags].sort().map(t => (
+														<Checkbox
+															key={t}
+															tag={t}
+															checked={selectedTags.includes(t)}
+															onChange={() =>
+																setSelectedTags(st =>
+																	st.includes(t)
+																		? st.filter(
+																				tag => tag !== t,
+																			)
+																		: [...st, t],
+																)
+															}
+														/>
+													))}
+												</div>
 											</div>
-										</div>
-									),
-								)}
-							</div>
+										),
+									)}
+								</div>
 
-							<div className="flex justify-end gap-4 px-4 mt-6">
-								<button
-									onClick={() => setIsOpen(false)}
-									className="block px-3 py-2 border text-primary-400 xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 border-primary-400">
-									Cancel
-								</button>
-								<Link
-									href={getTagsLink(selectedTags)}
-									onClick={() => setIsOpen(false)}
-									className="block px-3 py-2 text-white xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 hover:shadow-primary-400 bg-primary-400">
-									Save
-								</Link>
+								<div className="flex justify-end gap-4 px-4 mt-6">
+									<button
+										onClick={() => setIsOpen(false)}
+										className="block px-3 py-2 border text-primary-400 xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 border-primary-400">
+										Cancel
+									</button>
+									<Link
+										href={getTagsLink(selectedTags)}
+										onClick={() => setIsOpen(false)}
+										className="block px-3 py-2 text-white xs:text-sm sm:text-base lg:text-md font-montserrat-regular hover:scale-105 hover:shadow-primary-400 bg-primary-400">
+										Save
+									</Link>
+								</div>
 							</div>
-						</div>
-					</motion.div>
-				) : null}
-			</AnimatePresence>
+						</motion.div>
+					) : null}
+				</AnimatePresence>
+			</div>
 
 			{searchTags.map(t => (
 				<div
