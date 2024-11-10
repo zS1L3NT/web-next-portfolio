e7ec9dbcc15423f676839830cd2050dbd944e691
zS1L3NT Mac <dev@zectan.com>
2024-11-10 15:29:58 +0800

    ✨ wording update and add leetcode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,6 +87,23 @@ export default async function Page() {
 										<path d="M 40.34375 1.960938 L 37.859375 2.417969 L 41.1875 20.625 L 43.171875 20.324219 Z M 29.0625 6.664063 L 27.101563 8.078125 L 37.300781 23.035156 L 39.046875 21.796875 Z M 20.351563 15.507813 L 19.113281 17.703125 L 34.5 27 L 35.664063 25.003906 Z M 16.164063 25.171875 L 15.628906 27.402344 L 33.359375 31.894531 L 34 29.921875 Z M 9 29 L 9 47.984375 L 38.902344 48 L 38.902344 47.984375 C 38.933594 47.984375 39 29 39 29 L 37 29 L 37 46 L 11 46 L 11 29 Z M 15.171875 33.375 L 14.902344 35.339844 L 33 37 L 33.203125 35.035156 Z M 15 40 L 15 42 L 33 41.929688 L 33 40 Z" />
 									</svg>
 								</Link>
+								<Link
+									className="z-0 block"
+									href="https://leetcode.com/zs1l3nt"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<svg
+										className={cn(
+											svgClassName,
+											"p-1 my-1 xs:mx-2 xs:w-12 xs:h-12 sm:mx-3 sm:w-14 sm:h-14 md:mx-4 md:w-16 md:h-16",
+										)}
+										viewBox="0 0 16 16"
+									>
+										<title>LeetCode</title>
+										<path d="M 10.447266 0.265625 A 0.50005 0.50005 0 0 0 10.103516 0.41796875 L 5.65625 4.8671875 L 3.0957031 7.4257812 A 0.50005 0.50005 0 0 0 2.9785156 7.6035156 C 1.769869 8.9739016 1.7865696 11.063913 3.0957031 12.373047 L 5.65625 14.933594 C 7.0176322 16.294976 9.242133 16.294976 10.603516 14.933594 L 12.853516 12.683594 A 0.50063809 0.50063809 0 1 0 12.144531 11.976562 L 9.8945312 14.226562 C 8.9159134 15.20518 7.3418991 15.20518 6.3632812 14.226562 L 3.8027344 11.666016 C 2.8241166 10.687398 2.8241166 9.1114303 3.8027344 8.1328125 L 6.3632812 5.5742188 C 7.3418991 4.5956009 8.9159135 4.5956009 9.8945312 5.5742188 L 12.144531 7.8242188 A 0.50063784 0.50063784 0 1 0 12.853516 7.1171875 L 10.603516 4.8671875 C 9.9106907 4.174363 8.9943718 3.8431189 8.0820312 3.8554688 L 10.8125 1.125 A 0.50005 0.50005 0 0 0 10.447266 0.265625 z M 7.328125 9.4003906 A 0.50005 0.50005 0 1 0 7.328125 10.400391 L 14.228516 10.400391 A 0.50005 0.50005 0 1 0 14.228516 9.4003906 L 7.328125 9.4003906 z" />
+									</svg>
+								</Link>
 							</div>
 						</div>
 
@@ -120,10 +137,10 @@ export default async function Page() {
 								real-world problems.
 							</p>
 							<p>
-								I competed in the 2024 WorldSkills Championship in Lyon, France,
-								where I won a Bronze Medal representing Singapore. Preparing for
-								this competition pushed me to master a broad range of tools in the
-								Web Development space, which was both intellectually demanding and
+								I won a Bronze Medal in the WorldSkills Championship 2024 in Lyon,
+								France, in the Web Development skill trade. Preparing for this
+								competition pushed me to master a broad range of tools in the Web
+								Development space, which was both intellectually demanding and
 								rewarding.
 							</p>
 							<p>
